4a8e87050f9f6853a416d6f097c9f7d0d4e9dcd7
Luciara-Abreu <luciara.abreu@gmail.com>
2023-09-08 23:19:12 -0300

    add interfaces

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,13 @@
+interface IUserType {
+  id_pessoa?: string
+  nome: string
+  rg: string
+  cpf: string
+  data_nascimento: Date
+  data_admissao: Date
+  funcao: string
+}
+export default IUserType
+
+
+
